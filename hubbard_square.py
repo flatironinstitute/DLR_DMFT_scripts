296723838cf830502909024580960e4f82e1e701
Alexander Hampel <ahampel@flatironinstitute.org>
2021-08-02 17:42:22 -0400

    implement python only sumk function

--- hubbard_square.py
+++ hubbard_square.py
@@ -1,6 +1,6 @@
 #DMFT calculation for a hubbard model on a 2D square lattice
 #uses Triqs version 3.0.x
-
+import numpy as np
 from triqs.sumk import *
 from triqs.gf import *
 from triqs.lattice.tight_binding import TBLattice
@@ -25,7 +25,7 @@ outfile = 'U%.1f'%U
 p = {}
 # solver
 p["random_seed"] = 123 * mpi.rank + 567
-p["length_cycle"] = 200
+p["length_cycle"] = 100
 p["n_warmup_cycles"] = int(1e4)
 p["n_cycles"] = int(1e7/mpi.size)
 # tail fit
@@ -53,11 +53,48 @@ L = TBLattice(units = [(1, 0, 0) , (0, 1, 0)], hopping = hop, orbital_names= ran
 
 SK = SumkDiscreteFromLattice(lattice=L, n_points=nk)
 
-#function to extract density for a given mu, to be used by dichotomy function to determine mu
+def sumk(mu, Sigma, bz_weights, hopping, iw_vec = None):
+    '''
+    calc Gloc with mpi parallelism
+    '''
+    Gloc = Sigma.copy()
+    Gloc << 0.0+0.0j
+
+    n_orb = Gloc['up'].target_shape[0]
+
+    if not iw_vec:
+        iw_mat = np.array([iw.value * np.eye(n_orb) for iw in Gloc.mesh])
+    else:
+        iw_values =  np.array(list(Gloc.mesh.values()))[vals]
+        iw_mat = np.array([iw * np.eye(n_orb) for iw in iw_values])
+
+    mu_mat = mu * np.eye(n_orb)
+
+    #Loop on k points...
+    for wk, eps_k in zip(*[mpi.slice_array(A) for A in [bz_weights, hopping]]):
+        for block, gf in Gloc:
+            # these are now all python numpy arrays, here you can replace by any array like object shape [nw, norb,norb]
+            # numpy vectorizes the inversion automatically of shape [nw,orb,orb] over nw
+            # speed up factor 30, comparable to C++!
+            gf.data[:,:,:] += wk*np.linalg.inv(iw_mat[:] + mu_mat - eps_k - Sigma[block].data[:,:,:])
+
+    Gloc << mpi.all_reduce(mpi.world,Gloc,lambda x,y: x+y)
+    mpi.barrier()
+
+    return Gloc
+
+# old function to extract density for a given mu, to be used by dichotomy function to determine mu
 def Dens(mu):
     # calling the k sum here
     # github.com/TRIQS/triqs/blob/3.0.x/python/triqs/sumk/sumk_discrete.py#L73
     dens =  SK(mu = mu, Sigma = S.Sigma_iw).total_density()
+    if abs(dens.imag) > 1e-20:
+            mpi.report("Warning: Imaginary part of density will be ignored ({})".format(str(abs(dens.imag))))
+    return dens.real
+
+# function to determine density python only
+def dens_dlr(mu):
+    dens =  sumk(mu = mu, Sigma = S.Sigma_iw, bz_weights=SK.bz_weights, hopping=SK.hopping).total_density()
     if abs(dens.imag) > 1e-20:
             mpi.report("Warning: Imaginary part of density will be ignored ({})".format(str(abs(dens.imag))))
     return dens.real
@@ -92,9 +129,10 @@ for iteration_number in range(1,nloops+1):
 
     # determination of the next chemical potential via function Dens. Involves k summation
     start_time = timer()
-    mu, density = dichotomy(Dens, mu, density_required, 1e-4, .5, max_loops = 100, x_name="chemical potential", y_name="density", verbosity=3)
+    #mu, density = dichotomy(Dens, mu, density_required, 1e-4, .5, max_loops = 100, x_name="chemical potential", y_name="density", verbosity=3)
+    mu, density = dichotomy(dens_dlr, mu, density_required, 1e-4, .5, max_loops = 100, x_name="chemical potential", y_name="density", verbosity=3)
     mpi.barrier()
-    mpi.report('\nnumber of kpoints:', SK.n_kpts())
+    mpi.report('\nnumber of kpoints:'+str(SK.n_kpts()))
     mpi.report('time for calculating mu: {:.2f} s'.format(timer() - start_time))
     # calling the k sum here which you need to manually implement or change
     # but it is all python, even the invert is done on a numpy array
@@ -102,10 +140,10 @@ for iteration_number in range(1,nloops+1):
 
     # TODO step 3 write a new python function which replaces the SK() call which accepts a iw_vector and does the k sum only on these frequencies
     start_time = timer()
-    Gloc << SK(mu = mu, Sigma = S.Sigma_iw)
+    # Gloc << SK(mu = mu, Sigma = S.Sigma_iw)
+    Gloc << sumk(mu = mu, Sigma= S.Sigma_iw, bz_weights=SK.bz_weights, hopping=SK.hopping)
     mpi.barrier()
     mpi.report('time for k sum: {:.2f} s'.format(timer() - start_time))
-
     # TODO step 1
     # extract special Gloc(iw) points and do the DLR and back
     # iw_freq = [1, 10, 55, ..]
@@ -115,7 +153,7 @@ for iteration_number in range(1,nloops+1):
     # see if the result is the same
 
     nlat = Gloc.total_density().real # lattice density
-
+    mpi.report('Gloc density: {:.3f}'.format(nlat))
     # set starting guess for Sigma = U/2 at first iteration
     if it == 1:
         S.Sigma_iw << .5*U
@@ -130,7 +168,7 @@ for iteration_number in range(1,nloops+1):
     # TODO step 2:
     # replace this dyson eq by a fitting S.G_tau with a DLR fit and then extract Sigma
     # on DLR frequency grid to get rid of tail-fitting
-    # write Fourier() replacement method with DLR 
+    # write Fourier() replacement method with DLR
 
     #force self energy obtained from solver to be hermitian
     for name, s_iw in S.Sigma_iw:
